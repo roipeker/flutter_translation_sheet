8f93f1a930131f94ed60d3de4556601ca5a25855
Nicolas Lopez <unacorbatanegra@gmail.com>
2021-07-23 16:51:05 -0400

    package autogenerate version

--- lib/src/data/strings.dart
+++ lib/src/data/strings.dart
@@ -1,5 +1,7 @@
+import '../version.dart';
+
 class CliConfig {
   static const cliName = 'fts';
   static const packageName = 'flutter_translation_sheet';
-  static const version = '1.0.4+10';
+  static const version = packageVersion;
 }
