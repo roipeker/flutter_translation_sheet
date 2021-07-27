bc29f20cc409c2cd5b04e78c776871e1ba8233e6
Nicolas Lopez <unacorbatanegra@gmail.com>
2021-07-27 11:19:28 -0400

    local versio && global version

--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,15 @@
 import 'dart:io';
 
+
 import 'package:flutter_translation_sheet/src/runner.dart';
 
+
 Future<void> main(List<String> args) async {
+  await _checkEnviroment();
   exit(await FTSCommandRunner().run(args));
+}
+
+Future<void> _checkEnviroment() async {
+  CliConfig.isDev = Platform.script.toFilePath() == 'main.dart';
+  return;
 }
