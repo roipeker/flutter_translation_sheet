53accc0b3aa335929a0170d1a97eb8b41dbc6aa6
IsmailAlamKhan <mdismailalamkhan@gmail.com>
2021-07-29 04:32:00 +0600

    maybe fixed the issue of upgrade

--- lib/src/utils/version.dart
+++ lib/src/utils/version.dart
@@ -15,7 +15,8 @@ Future<void> upgrade() async {
     trace(green('Upgrading fts....\n'));
     final result =
         'flutter pub global activate flutter_translation_sheet'.start(
-      terminal: true,
+      // terminal: true,
+      runInShell: true,
       progress: Progress(
         (e) => trace(yellow(e, bold: false)),
         stderr: printerr,
@@ -88,6 +89,7 @@ Future<void> checkUpdate([bool fromCommand = true]) async {
   }
 
   try {
+    /*
     final latest = await _checkLatestVersion();
     if (latest == null) {
       error('cannot fetch the latest version');
@@ -126,6 +128,7 @@ Future<void> checkUpdate([bool fromCommand = true]) async {
         '\n___________________________________________________\n\n',
       ),
     );
+    */
     final result = confirm(
       yellow('Would you like update to the last version?'),
       defaultValue: true,
