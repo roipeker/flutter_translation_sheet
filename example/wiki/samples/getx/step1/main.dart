69e0879a67fdae2c62d424badfc0c2481166dbc3
Roi Peker <roipeker@gmail.com>
2021-08-09 19:32:14 -0300

    Create main.dart
    getx sample template for Wiki

---
+++ example/wiki/samples/getx/step1/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePageView(),
+    );
+  }
+}
+
+class HomePageController extends GetxController {
+  final _counter = 0.obs;
+  String get counterText => '${_counter.value}';
+  void increment() {
+    _counter.value++;
+  }
+}
+
+class HomePageView extends StatelessWidget {
+  const HomePageView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<HomePageController>(
+      init: HomePageController(),
+      global: false,
+      builder: (controller) => Scaffold(
+        appBar: AppBar(
+          title: Text('A beautiful counter'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('You have pushed the button this many times:'),
+              Obx(
+                    () => Text(
+                  controller.counterText,
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: controller.increment,
+          tooltip: 'Increment',
+          child: Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
